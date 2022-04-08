52ba8ba87a18a4a76efe516bc94e59674d92545b
crisepu <csepulvedavenegas@gmail.com>
2022-04-08 15:09:45 +0000

    exercise finish with validations

--- validation.js
+++ validation.js
@@ -14,6 +14,17 @@ loginForm.addEventListener('submit', (e) => {
     let inputPostalCode = document.querySelector("#inputPostalCode");
     let inputMessage = document.querySelector("#inputMessage");
     let principalAlert = document.querySelector("#bigAlert");
+
+    let cardAlert = document.querySelector("#cardAlert");
+    let cvcAlert = document.querySelector("#cvcAlert");
+    let amountAlert = document.querySelector("#amountAlert");
+    let firstNameAlert = document.querySelector("#firstNameAlert");
+    let lastNameAlert = document.querySelector("#lastNameAlert");
+    let cityAlert = document.querySelector("#cityAlert");
+    let stateAlert = document.querySelector("#stateAlert");
+    let postalCodeAlert = document.querySelector("#postalCodeAlert");
+    let messageAlert = document.querySelector("#messageAlert");
+   
     
     let patternCard = /5[1-5][0-9]{14}$/; // expresion regular mastercard
     let patternCVC = /^[0-9]{3,4}$/;
@@ -29,51 +40,87 @@ loginForm.addEventListener('submit', (e) => {
     if (!patternCard.test(inputCard.value)) {
         inputCard.classList.add("alert-danger");
         activePrincipalAlert();
+        cardAlert.style.display = "";
         flag = false;
+    } else {
+        inputCard.classList.remove("alert-danger");
+        cardAlert.style.display = "none";
     }
     if (!patternCVC.test(inputCVC.value)) {
         inputCVC.classList.add("alert-danger");
         activePrincipalAlert();
+        cvcAlert.style.display = "";
         flag = false;
+    } else {
+        inputCVC.classList.remove("alert-danger");
+        cvcAlert.style.display = "none";
     }
     if (!patternAmount.test(inputAmount.value)) {
         inputAmount.classList.add("alert-danger");
         activePrincipalAlert();
+        amountAlert.style.display = "";
         flag = false;
+    } else {
+        inputAmount.classList.remove("alert-danger");
+        amountAlert.style.display = "none";
     }
     if (!patternNameCity.test(inputFirstName.value)) {
         inputFirstName.classList.add("alert-danger");
         activePrincipalAlert();
+        firstNameAlert.style.display = "";
         flag = false;
+    } else {
+        inputFirstName.classList.remove("alert-danger");
+        firstNameAlert.style.display = "none";
     }
     if (!patternNameCity.test(inputLastName.value)) {
         inputLastName.classList.add("alert-danger");
         activePrincipalAlert();
+        lastNameAlert.style.display = "";
         flag = false;
+    } else {
+        inputLastName.classList.remove("alert-danger");
+        lastNameAlert.style.display = "none";
     }
     if (!patternNameCity.test(inputCity.value)) {
         inputCity.classList.add("alert-danger");
         activePrincipalAlert();
+        cityAlert.style.display = "";
         flag = false;
+    } else {
+        inputCity.classList.remove("alert-danger");
+        cityAlert.style.display = "none";
     }
     if (!patternPostalCode.test(inputPostalCode.value)) {
         inputPostalCode.classList.add("alert-danger");
         activePrincipalAlert();
+        postalCodeAlert.style.display = "";
         flag = false;
+    } else {
+        inputPostalCode.classList.remove("alert-danger");
+        postalCodeAlert.style.display = "none";
     }
-    if (!patternMessage.test(inputMessage.value)) {
+    if (inputMessage.value == "") {
         inputMessage.classList.add("alert-danger");
         activePrincipalAlert();
+        messageAlert.style.display = "";
         flag = false;
+    } else {
+        inputMessage.classList.remove("alert-danger");
+        messageAlert.style.display = "none";
     }
-    if (inputState == "Pick a State") {
+    if (inputState.value == "") {
         inputState.classList.add("alert-danger");
         activePrincipalAlert();
+        stateAlert.style.display = "";
         flag = false;
+    } else {
+        inputState.classList.remove("alert-danger");
+        stateAlert.style.display = "none";
     }
     //console.log(inputCreditCard.value);
     //Validacion Card
-    
+    console.log(inputMessage.value)
 
     
     
